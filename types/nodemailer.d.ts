7aac890fb1acd7295b0e1bc5935a4cbb5be0f6b0
st214023 <matteo.tomaselli449@edu.unito.it>
2026-03-15 22:07:22 +0100

    nodemailer

---
+++ types/nodemailer.d.ts
@@ -0,0 +1,23 @@
+declare module 'nodemailer' {
+  export interface TransportOptions {
+    service?: string;
+    auth?: {
+      user?: string;
+      pass?: string;
+    };
+  }
+
+  export interface MailOptions {
+    from?: string;
+    to?: string;
+    subject?: string;
+    html?: string;
+  }
+
+  export interface Transporter {
+    sendMail(mailOptions: MailOptions): Promise<any>;
+  }
+
+  function createTransport(options: TransportOptions): Transporter;
+  export { createTransport };
+}
